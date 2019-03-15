8b232509bdaad6421e8ebfa0ef493dfece75f518
Coleton Wheeler <cpwheele@rs-f251-m02.local>
2019-03-15 10:40:59 -0500

    Push to remote

--- AdButton/ViewController.swift
+++ AdButton/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Create outlets
+    @IBOutlet weak var button: UIImageView!
+    @IBOutlet weak var coinTotal: UILabel!
+    
+    //Declare variables on runtime
+    var coins = 0
+    
+    //Load on runtime
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Sets the image to a button
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.imageTapped(gesture:)))
+        button.addGestureRecognizer(tapGesture)
+        button.isUserInteractionEnabled = true
     }
-
+    
+    //When button is clicked
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        if (gesture.view as? UIImageView) != nil {
+            //Ad showing goes here
+            coinRewards()
+        }
+    }
+    
+    //Add coins after watching ad
+    func coinRewards() {
+        coins += 1
+        coinTotal.text = "Coins: " + String(coins)
+    }
 
 }
 
